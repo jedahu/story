67f2d067b42d61757e40eafa2d77c5e4cdcc7c39
Jeremy Hughes <jedahu@gmail.com>
2011-11-22 21:10:30 +1300

    progress...

--- project.clj
+++ project.clj
@@ -1,8 +1,68 @@
+; # A programming story
+;
+; This is the story of a small program that parses source code. It converts
+; comments into HTML (via [Markdown]) and nicely highlights code (using
+; [SyntaxHighlighter]).
+;
+; [MarkDown]: https://github.com/sirthias/pegdown
+; [SyntaxHighlighter]: http://alexgorbatchev.com/SyntaxHighlighter/
+
 (defproject
   me.panzoo/story "0.0.1-SNAPSHOT"
 
-  :description "FIXME: write description"
+  :description "A literate programming tool."
+
+; The program is writtent in the [Clojure] language and uses the [pegdown]
+; implementation of Markdown with a number of its extensions
+; [enabled](#pegdown-extensions).
+;
+; [Clojure]: http://clojure.org
+; [pegdown]: http://daringfireball.net/projects/markdown/
 
   :dependencies
   [[org.clojure/clojure "1.3.0"]
-   [org.pegdown/pegdown "1.1.0"]])
+   [org.pegdown/pegdown "1.1.0"]
+   [com.google.javascript/closure-compiler "r1592"]
+   [org.clojure/tools.cli "0.2.1"]]
+  
+  :main me.panzoo.story)
+
+; ## Synopsis
+;
+; To use this program you need to know four things: the single-line comment
+; syntax of your source-code, Markdown, the story syntax for anchors, and the
+; story syntax for includes.
+;
+; Comment blocks that are flush with the left margin are parsed and rendered
+; using Markdown. Anchors are comment lines whose content is '`@<anchor-id>`'.
+; To include another source file add a comment line whose content is
+; '`%include <file-path>`'.
+;
+; By default this program is set up to process a Lisp like language with single
+; semi-colon comment tokens. A small source example with anchors, includes,
+; and wiki links might look like this:
+;
+; <div class=code><pre class='brush: clojure'>
+; ; # My awesome program
+; ;
+; ; Introductory paragraph. Fast forward to the [[code]].
+; ;
+; ; Blah blah blah.
+;
+; ;@code
+; (defn hello []
+;   (println "Hello world!"))
+; 
+; ; See the code [again!](#code)
+;
+; ;%include fibonacci.clj
+; </pre></div>
+;
+; ### Command line usage
+;
+; `java -jar story.jar [-c|--comment <token>] [-o|-out <outfile>] <input>`
+;
+; For more detail run the program with the `--help` option or have a look at
+; the [[commandline entry point]].
+
+;%include src/me/panzoo/story.clj
--- src/me/panzoo/story.clj
+++ src/me/panzoo/story.clj
@@ -1,57 +1,151 @@
-; # A literate programming story
+; # The code
 ;
-; This is the story of a small program that parses other programs. It converts
-; their comments into HTML (using the [Pegdown][1] implementation of
-; [Markdown][2]) and nicely highlights their code
-; (using [SyntaxHighlighter][3]).
-;
-; [1]: https://github.com/sirthias/pegdown
-; [2]: http://daringfireball.net/projects/markdown/
-; [3]: http://alexgorbatchev.com/SyntaxHighlighter/
-;
-; The usual stuff comes first. We need to read and write to files, manipulate
-; strings, and parse Markdown.
+; The usual stuff comes first. This code needs to read and write to files,
+; manipulate strings, and parse Markdown.
 
 (ns me.panzoo.story
+  (:refer-clojure :exclude [comment])
   (:require
     [clojure.java.io :as io]
-    [clojure.string :as string])
+    [clojure.string :as string]
+    [clojure.tools.cli :as cli])
   (:import
-    [org.pegdown PegDownProcessor Extensions]))
+    [org.pegdown PegDownProcessor Extensions
 
-; A couple of utility functions to make it easier to iterate side-effects
-; through a collection and access resources.
+; Internal links (to element ids) are nice to have. These imports will be used
+; to create a modified LinkRenderer.
 
-(defn- each
+     LinkRenderer LinkRenderer$Rendering]
+    [org.pegdown.ast WikiLinkNode])
+  
+  (:gen-class))
+
+; ## Utilities
+;
+; A few utility functions to make it easier to iterate side-effects through a
+; collection, access resources, encode a string for use as an id or fragment
+; identifier, and bind an output stream to `*out*` over a lexical scope.
+
+(defn each
   "Apply f to each item of coll in order for side effects only. Returns nil."
   [f coll]
   (doseq [c coll]
     (f c)))
 
 (defn slurp-resource
-  "Stolen from marginalia."
+  "Get the complete contents of a Java resource."
   [resource-name]
   (-> (.getContextClassLoader (Thread/currentThread))
     (.getResourceAsStream resource-name)
     (java.io.InputStreamReader.)
     (slurp)))
 
-; Two objects need to be accessed by a number of functions: a regular
-; expression that matches the beginning of a commented line, and a
-; PegDownProcessor. Neither object will be mutated after the initial setup.
+(defn encode-anchor [s]
+  (java.net.URLEncoder/encode (.replace s " " "-") "UTF-8"))
 
-(def ^:dynamic *single-comment* #"^; ?")
-(def ^:dynamic *processor* nil)
+(defmacro with-out-stream [out & body]
+  `(with-open [w# (io/writer ~out)]
+     (binding [*out* w#]
+       ~@body)))
 
-; Each line of a source file will be either code or comment.
+; ## Top level variables
+;
+; A few objects need to be accessed by a number of functions: regular
+; expressions that match the beginning of commented lines, SyntaxHighlighter
+; options, a PegDownProcessor, and a modified LinkRenderer. Of these, the
+; processor and renderer are never dynamically bound. The other variables are
+; bound once for the lifetime of the program, so with the rest of the program
+; they can also be considered constant.
+;
+; ### Parsing variables
+
+(def ^:dynamic *single-comment*
+  "A string containing a single comment token."
+  ";")
+
+(def comment
+  "A regular expression matching the beginning of a commented line."
+  (delay (re-pattern (str "^\\Q" *single-comment* "\\E ?"))))
+
+(def anchor
+  "A regular expression matching the beginning of an anchor line."
+  (delay (re-pattern (str @comment "@ *"))))
+
+(def include
+  "A regular expression matching the beginning of an include line."
+  (delay (re-pattern (str @comment "%include +"))))
+
+; ### Style variables
+
+(def ^:dynamic *brush*
+  "Path to a SyntaxHighlighter brush file."
+  nil)
+
+(def ^:dynamic *theme*
+  "Path to a SyntaxHighlighter theme file."
+  nil)
+
+(def ^:dynamic *language*
+  "A language string for use with SyntaxHighlighter."
+  "clojure")
+
+(def ^:dynamic *stylesheet*
+  "Path to a stylesheet file."
+  nil)
+
+; ### Pegdown variables
+
+;@pegdown-extensions
+(def processor
+  "A PegDownProcessor set up with the following extensions:
+  AUTOLINKS
+  SMARTYPANTS
+  FENCED_CODE_BLOCKS
+  DEFINITIONS
+  WIKILINKS"
+  (PegDownProcessor. (bit-or
+                       (. Extensions AUTOLINKS)
+                       (. Extensions SMARTYPANTS)
+                       (. Extensions FENCED_CODE_BLOCKS)
+                       (. Extensions DEFINITIONS)
+                       (. Extensions WIKILINKS))))
+
+(def link-renderer
+  "A pegdown LinkRenderer that renders wiki links as links to internal
+  document fragments rather than external HTML pages."
+  (proxy [LinkRenderer] []
+    (render
+      ([node]
+       (if (instance? WikiLinkNode node)
+         (try
+           (LinkRenderer$Rendering.
+             (str
+               "#"
+               (encode-anchor (.. node (getText))))
+             (.getText node))
+           (catch java.io.UnsupportedEncodingException _
+             (throw (IllegalStateException.))))
+         (proxy-super render node)))
+      ([node text]
+       (proxy-super render node text))
+      ([node url title text]
+       (proxy-super render node url title text)))))
+
+; ## Parsing
+;
+; Each line of a source file will be either code, comment, anchor, or include.
 
 (defn- classify-line
   "Classify a line as :code or :comment. Return a pair of the classification
   and line string, sans leading comment tokens in the case of a comment."
   [line]
-  (if-let [s (re-find *single-comment* line)]
-    [:comment (.substring line (count s))]
-    [:code line]))
+  (if-let [s (re-find @include line)]
+    [:include (.substring line (count s))]
+    (if-let [s (re-find @anchor line)]
+      [:anchor (.substring line (count s))]
+      (if-let [s (re-find @comment line)]
+        [:comment (.substring line (count s))]
+        [:code line]))))
 
 ; Adjacent lines of the same classification need to be gathered together into
 ; a single string.
@@ -59,7 +153,9 @@
 (defn- gather-lines- [lines]
   (lazy-seq
     (let [classfn (first (first lines))
-          [same tail] (split-with #(= classfn (first %)) lines)
+          [same tail] (if (#{:comment :code} classfn)
+                        (split-with #(= classfn (first %)) lines)
+                        (split-at 1 lines))
           text (string/join "\n" (map second same))]
       (when classfn (cons [classfn text] (gather-lines- tail))))))
 
@@ -67,83 +163,154 @@
   (gather-lines- (map classify-line lines)))
 
 ; The result of gathering is a list of pairs of the form
-; `[<classification> <string>]`. The method that transforms each classified
+; `[<classification> <string>]`.
+
+; ## Rendering
+;
+; The method that transforms each classified
 ; chunk into HTML, dispatches on the classification.
 
 (defmulti html<- first)
 
-; Comments are run through the Markdown processor and then wrapped in a
-; `section`.
-
-(defmethod html<- :comment [[_ text]]
-  (str "<section class='comment'>\n"
-       (.markdownToHtml *processor* text)
-       "</section>\n"))
-
-; Code chunks are wrapped in a `pre` with the correct incantation for
-; SyntaxHighlighter in its `class` attribute.
-
-(defmethod html<- :code [[_ text]]
-  (string/join "\n" ["<div class='code'><pre class='brush: clojure'>"
-                     (string/escape
-                       text
-                       {\< "&lt;"
-                        \> "&gt;"
-                        \& "&amp;"})
-                     "</pre></div>"]))
-
-; The SyntaxHighlighter code is inlined into the output HTML.
-
-(defn- inline-js [path]
-  (str "<script>\n" (slurp-resource path) "</script>"))
-
-(defn- inline-css [path]
-  (str "<style>\n" (slurp-resource path) "</style>"))
-
-; Code, comments, and supporting HTML are written to an output file.
-
-(defn- render-to-file [sections path]
-  (with-open [w (io/writer path)]
-    (binding [*out* w]
-      (each
-        println
-        ["<!doctype html>"
-         "<meta charset=utf-8>"
-         (inline-js "XRegExp.js")
-         (inline-js "shCore.js")
-         (inline-js "shBrushClojure.js")
-         (inline-css "shThemeEclipse.css")
-         (inline-css "shClojureExtra.css")
-         "<style>"
-         "div.code { background-color: #f5f5ff }"
-         "div.code { border: solid #e5e5ee 1px }"
-         "</style>"])
-      (each println (map html<- sections))
-
-; SyntaxHighlighter's defaults are not suitable, so they get tweaked.
-
-      (each
-        println
-        ["<script>"
-         "SyntaxHighlighter.defaults['toolbar'] = false;"
-         "SyntaxHighlighter.defaults['smart-tabs'] = false;"
-         "SyntaxHighlighter.defaults['gutter'] = false;"
-         "SyntaxHighlighter.defaults['unindent'] = false;"
-         "SyntaxHighlighter.all();"
-         "</script>"]))))
-
 ; Lines of source code are read lazily by `line-seq` and gathered lazily by
-; `gather-lines`; along with printing each chunk of comment or code to an
+; `gather-lines`, Along with printing each chunk of comment or code to an
 ; output stream, this ensures that the maximum memory used by this program will
 ; be determined by the largest comment or code chunk and not the total size of
 ; the source file.
 
-(defn process-file [in-path out-path]
-  (with-open [r (io/reader in-path)]
-      (render-to-file (gather-lines (line-seq r)) out-path)))
+(defn render-file [tag path]
+  (with-open [r (io/reader path)]
+    (println (str "<" tag ">"))
+    (each html<- (gather-lines (line-seq r)))
+    (println (str "</" tag ">"))))
 
-(defmacro with-processor [& body]
-  `(binding [*processor* (PegDownProcessor. (bit-or
-                                              (. Extensions AUTOLINKS)
-                                              (. Extensions SMARTYPANTS)))]
-     ~@body))
+; Code chunks are wrapped in a `pre` with the correct incantation for
+; SyntaxHighlighter in its `class` attribute; comments are run through the
+; Markdown processor; anchors become hrefless HTML anchors; and includes wrap
+; the result of parsing and rendering the file they point to in `section` tags.
+
+(defmethod html<- :code [[_ text]]
+  (when-not (string/blank? text)
+    (println (str "<div class=code><pre class='brush: " *language* "'>")
+             (string/escape
+               text
+               {\< "&lt;"
+                \> "&gt;"
+                \& "&amp;"})
+             "</pre></div>")))
+
+(defmethod html<- :comment [[_ text]]
+  (println (.markdownToHtml processor text link-renderer)))
+
+(defmethod html<- :anchor [[_ text]]
+  (println (str "<a id='" (encode-anchor text) "'></a>")))
+
+(defmethod html<- :include [[_ path]]
+  (render-file "section" path))
+
+; ## Look and feel
+;
+; All javascript and css is inlined in the output.
+
+(defn- inline-js [s]
+  (println "<script>" s "</script>"))
+
+(defn- inline-css [s]
+  (println "<style>" s "</style>"))
+
+; The default SyntaxHighlighter brush and theme (clojure and eclipse) can be
+; overridden on the commandline, and an additional stylesheet can be included
+; too.
+
+(defn inline-brush []
+  (inline-js
+    (if *brush*
+      (slurp *brush*)
+      (slurp-resource "shBrushClojure.js"))))
+
+(defn inline-theme []
+  (inline-css
+    (if *theme*
+      (slurp *theme*)
+      (slurp-resource "shThemeEclipse.css"))))
+
+(defn inline-stylesheet []
+  (when *stylesheet*
+    (inline-css *stylesheet*)))
+
+; The resulting look-and-feel resources are included together.
+
+(defn look-and-feel []
+  (inline-js (slurp-resource "XRegExp.js"))
+  (inline-js (slurp-resource "shCore.js"))
+  (inline-brush)
+  (inline-theme)
+  (inline-css (slurp-resource "page.css"))
+  (inline-stylesheet))
+
+; SyntaxHighlighter's defaults are not suitable, so they are tweaked.
+
+(defn syntax-highlighter-setup []
+  (inline-js
+    (str "SyntaxHighlighter.defaults['toolbar'] = false;"
+         "SyntaxHighlighter.defaults['smart-tabs'] = false;"
+         "SyntaxHighlighter.defaults['gutter'] = false;"
+         "SyntaxHighlighter.defaults['unindent'] = false;"
+         "SyntaxHighlighter.all();")))
+    
+; ## Output
+;
+; The output is HTML5 which is why no `html`, `head`, or `body` tags are
+; present. The order of things in the output file is fairly normal for HTML:
+; style and behaviour information first, followed by visible content, followed
+; by the javascript entry-point.
+
+(defn- render-files [paths]
+  (println
+    "<!doctype html>"
+    "<meta charset=utf-8>")
+  (look-and-feel)
+
+; Multiple input files are rendered to a single stream. The content of each
+; file's rendering is wrapped in its own `article` tags.
+
+  (each (partial render-file "article") paths)
+  (syntax-highlighter-setup))
+
+; The output stream can be a file or standard output or anything
+; `clojure.java.io/writer` can handle.
+
+(defn process-files [in-paths out]
+  (with-out-stream out
+    (render-files in-paths)))
+
+; ## Commandline
+;
+; Use this program from the commandline like so.
+
+(def usage "Usage: java -jar story.jar [options] <input-files>")
+
+;@commandline entry point
+(defn -main [& args]
+  (let [[amap tail banner]
+        (cli/cli args
+                 ["-c" "--comment" "Comment syntax" :default ";"]
+                 ["-b" "--brush" "SyntaxHighlighter brush file"]
+                 ["-t" "--theme" "SyntaxHighlighter theme file"]
+                 ["-l" "--language" "SyntaxHighlighter language"]
+                 ["-s" "--stylesheet" "A stylesheet file to include"]
+                 ["-h" "--help" "Show this help" :default false :flag true])]
+    (if (or (not (seq tail)) (:help amap))
+      (do (println usage banner)
+        (System/exit 1))
+      (binding [*single-comment* (:comment amap)
+                *brush* (:brush amap)
+                *theme* (:theme amap)
+                *language* (or (:language amap) *language*)
+                *stylesheet* (:stylesheet amap)]
+
+; When no output file is given, the program renders to standard-out.
+
+        (if (= 1 (count tail))
+          (process-files tail *out*)
+          (process-files (butlast tail) (last tail)))))))

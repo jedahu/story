a6813118768fe3f16e0d3b475dcdb898421c8c91
Jeremy Hughes <jedahu@gmail.com>
2011-12-13 11:33:30 +1300

    page.css, page.js: make test sections togglable

--- resources/page.js
+++ resources/page.js
@@ -26,6 +26,47 @@ function me_panzoo_story_setup() {
 
   var h1s = document.getElementsByTagName('h1');
   if (h1s.length > 0) document.title = h1s[0].innerText;
+
+// Test code is hidden by default.
+  var makeToggle = function(test) {
+    var x = document.createElement('div');
+    x.setAttribute('class', 'test-toggle');
+    x.style.display = 'block';
+    x.innerText = '?';
+    x.onclick = function() {
+      if (test.style.display == 'none') {
+        test.style.display = 'block';
+      } else {
+        test.style.display = 'none';
+      }
+    };
+    return x;
+  };
+  var tests = document.querySelectorAll('div.test');
+  for (var i = 0; i < tests.length; ++i) {
+    console.log('test', i, tests[i]);
+    var test = tests[i];
+    var tog = makeToggle(test);
+    test.style.display = 'none';
+    test.parentNode.insertBefore(tog, test);
+  }
+
+// A master toggle is created if there is more than one test section.
+  if (tests.length > 1) {
+    var masterTog = document.createElement('div');
+    masterTog.setAttribute('id', 'master-toggle');
+    masterTog.innerText = '? show all';
+    masterTog.onclick = function() {
+      var show = masterTog.innerText == '? show all';
+      var display = show ? 'block' : 'none';
+      masterTog.innerText = show ? '? hide all' : '? show all';
+      for (var i = 0; i < tests.length; ++i) {
+        tests[i].style.display = display;
+      }
+    };
+    var e = document.querySelector('h1').nextSibling;
+    e.parentNode.insertBefore(masterTog, e);
+  }
 }
 
 me_panzoo_story_setup();
